498931e63be70a1d6e484d64f5f5e7b63e46a0f3
Neeraj Velpula <108074211+neerajhon@users.noreply.github.com>
2025-04-23 18:59:48 +0530

    Add files via upload

---
+++ refresh/extract.py
@@ -0,0 +1,12 @@
+import pandas as pd
+from sqlalchemy import create_engine
+
+def extract_data():
+    engine = create_engine('postgresql://postgres:postgres@localhost:5433/batch_db')
+    df = pd.read_sql("SELECT * FROM public.customer_behavior", engine)
+    print("✅ Data extracted from PostgreSQL:")
+    print(df.head())  # Display first 5 rows
+    return df
+
+if __name__ == "__main__":
+    extract_data()
